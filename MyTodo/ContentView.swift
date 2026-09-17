0f384f69191e2f91f46c27465f8d422c994bd019
TNTTheLagger <tntcsatornaja@gmail.com>
2026-09-17 12:22:51 +0200

    Initial Commit

---
+++ MyTodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTodo
+//
+//  Created by Guest User on 2026. 09. 17..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTodo/MyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoApp.swift
+//  MyTodo
+//
+//  Created by Guest User on 2026. 09. 17..
+//
+
+import SwiftUI
+
+@main
+struct MyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
